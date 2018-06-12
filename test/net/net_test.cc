74e63c91d1f9a880bee1919c916925634378128b
Oliver Michel <oliver.michel@editum.de>
2018-06-11 18:35:57 -0600

    add empty packet_header and net tests

---
+++ test/net/net_test.cc
@@ -0,0 +1,10 @@
+
+#include <catch.h>
+#include <om/om.h>
+
+using namespace om;
+
+TEST_CASE("net", "[net]")
+{
+
+}
---
+++ test/net/packet_header_test.cc
@@ -0,0 +1,10 @@
+
+#include <catch.h>
+#include <om/om.h>
+
+using namespace om;
+
+TEST_CASE("net::packet_header", "[net][packet_header]")
+{
+
+}
